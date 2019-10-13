def0c1ae7f2a45c682b65b6e1bb4a5c046b0fd00
Caleb201 <anyaechecaleb00@gmail.com>
2019-10-12 20:41:43 -0500

    Implemented condition variables directly

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -1,6 +1,7 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.LinkedList; // +hy+
 
 /**
  * An implementation of condition variables that disables interrupt()s for
@@ -22,6 +23,8 @@ public class Condition2 {
      */
     public Condition2(Lock conditionLock) {
         this.conditionLock = conditionLock;
+
+        waitQueue = new LinkedList<KThread>();  // +hy+
     }
 
     /**
@@ -35,6 +38,13 @@ public class Condition2 {
 
         conditionLock.release();
 
+        boolean intStatus = Machine.interrupt().disable();  // +hy+
+
+	    waitQueue.add(KThread.currentThread()); // +hy+
+	    KThread.sleep();    // +hy+
+    
+	    Machine.interrupt().restore(intStatus); // +hy+
+
         conditionLock.acquire();
     }
 
@@ -44,6 +54,13 @@ public class Condition2 {
      */
     public void wake() {
         Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        boolean intStatus = Machine.interrupt().disable();  // +hy+
+
+	    ((KThread)waitQueue.removeFirst()).ready();     // +hy+
+    
+        Machine.interrupt().restore(intStatus);         // +hy+
+        
     }
 
     /**
@@ -52,7 +69,66 @@ public class Condition2 {
      */
     public void wakeAll() {
         Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        while (!waitQueue.isEmpty()) // +hy+
+	        wake();                  // +hy+
     }
 
+    // Test code added by hy
+    private static class Condition2Test implements Runnable {
+        Condition2Test(Lock lock, Condition2 condition) {
+            this.condition = condition;
+            this.lock = lock;
+        }
+        
+        public void run() {
+            lock.acquire();
+    
+            System.out.print(KThread.currentThread().getName() + " acquired lock\n");	
+            condition.sleep();
+            System.out.print(KThread.currentThread().getName() + " acquired lock again\n");	
+    
+            lock.release();
+            System.out.print(KThread.currentThread().getName() + " released lock \n");	
+        }
+    
+        private Lock lock; 
+        private Condition2 condition; 
+    }
+    
+    /**
+     * Test if this module is working.
+     */
+    public static void selfTest() {
+
+        System.out.print("Enter Condition2.selfTest\n");	
+
+        Lock lock = new Lock();
+        Condition2 condition = new Condition2(lock); 
+
+        KThread t[] = new KThread[10];
+        for (int i=0; i<10; i++) {
+                t[i] = new KThread(new Condition2Test(lock, condition));
+                t[i].setName("Thread" + i).fork();
+        }
+
+        KThread.yield();
+        
+        lock.acquire();
+
+        System.out.print("condition.wake();\n");	
+        condition.wake();
+
+        System.out.print("condition.wakeAll();\n");	
+        condition.wakeAll();
+
+        lock.release();
+
+        System.out.print("Leave Condition2.selfTest\n");	
+
+        t[9].join();
+        
+    }
     private Lock conditionLock;
+    private LinkedList<KThread> waitQueue;  // +hy+
 }
